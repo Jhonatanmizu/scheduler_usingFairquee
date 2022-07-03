551285535a6b5cfca4aea3c9c9fcd34b5657942a
jhonatanmizu <natanjesuss20@gmail.com>
2022-07-03 17:22:49 -0300

    Find bug in remove

--- src/main.js
+++ src/main.js
@@ -1,36 +1,39 @@
 const prompt = require("prompt-sync")();
 
 let process = [];
-// let time = 1;
 let totalQuantum = 0;
 let quantum = 3;
-let quantOfProcess;
+let quantOfProcess = 0;
 let finishedProcess = [];
+
 /**
- * It creates a process object with a random id, a name, a process size, and a running status
- * @param name - The name of the process.
- * @param processSize - The size of the process.
+ * It generates a process with a random id, a name, a process size, a create time, and a used time
+ * @param name - The name of the process
+ * @param processSize - The size of the process, which is the amount of time it takes to execute.
  * @returns A process model object.
  */
 function generateProcess(name, processSize) {
   let processModel = {
-    id: Math.floor(Math.random() * 10),
-    name: name,
-    running: false,
-    processSize: processSize,
-    executedTime: 0,
-    restTime: processSize,
-    createTime: totalQuantum,
-    used: 0,
-    shouldUse: 0,
+    id: process.length,
+    name: name, // nome do processo
+    running: false, // se está  rodando
+    processSize: processSize, // tamanho do processo
+    executedTime: 0, // momento no qual é executado
+    restTime: processSize, // tempo restante do processo
+    createTime: totalQuantum, // momento no qual foi criado
+    used: 0, // quanto usou
+    shouldUse: 0, // quanto deveria usar
   };
   totalQuantum += quantum;
 
   return processModel;
 }
+/**
+ * The above function creates a process.
+ */
 function createProcess() {
   quant = prompt("Digite a quantidade de process : ");
-  for (let i = 1; i <= Number(quant); i++) {
+  for (let i = 0; i <= Number(quant); i++) {
     let pName = "processo : ";
     process.push(
       generateProcess(pName + i, Math.floor(Math.random() * 10 + 1))
@@ -42,77 +45,74 @@ function createProcess() {
  * @param process - the process to be removed
  */
 function removeProcess(process) {
-  process.forEach((pr) => {
-    console.log("O PR antes", pr);
-    if (pr.restTime <= 0) {
-      pr.running = false;
-      console.log("O PR dps, pr time", pr, pr.restTime);
-
-      let index = process.findIndex((pr) => pr == process);
-      finishedProcess.push(process.splice(index, 1));
-      console.log("PROCESSOS REMOVIDOS", finishedProcess);
-    }
-  });
+  // console.log("quantidade de processos", quantOfProcess);
+  // if (process.length > 0) {
+  //   process.forEach((pr, index) => {
+  //     console.log("LOG", pr);
+  //     console.log("processos", process);
+  //     if (pr.restTime <= 0) {
+  //       pr.running = false;
+  //       // let index = process.findIndex((pr) => pr.id );
+  //       // finishedProcess.push(process.splice(index, 1));
+  //       finishedProcess.push(pr);
+  //     }
+  //   });
+  // }
+  finishedProcess = process.map((pr) => pr.restTime < 0);
+  console.log("FINIF", finishedProcess);
 }
 /**
  * It sorts an array of objects by the value of the property `shouldUse`
  * @param process - The array of processes to sort.
  */
 function sortByShouldUse(process) {
-  console.log("ORDENANDO");
   process = process.sort((a, b) => {
     return b.shouldUse - a.shouldUse;
   });
-  console.log(process);
+  // console.log(process);
 }
+/**
+ * It executes the process, and then sorts the process by the shouldUse property
+ * @returns the process array with the updated values.
+ */
 function executeProcess() {
   quantOfProcess = process.length;
   if (process.length == 0) return;
   process = process.map((pr) => {
-    if (pr.restTime >= 0) {
+    if (pr.restTime > 0) {
       ex = {
         id: pr.id,
         name: pr.name,
         running: true,
         processSize: pr.processSize,
-        executedTime: pr.executedTime + quantum,
+        executedTime: totalQuantum,
         restTime: pr.restTime - quantum,
         createTime: pr.createTime,
         used: pr.used + quantum,
         shouldUse: pr.used / (totalQuantum / quantOfProcess),
       };
       totalQuantum += quantum;
-      console.log(pr.used / (totalQuantum / quantOfProcess));
       return ex;
     }
-
-    // if (!pr.running) {
-    //   ex = {
-    //     id: pr.id,
-    //     name: pr.name,
-    //     running: pr.running,
-    //     processSize: pr.processSize,
-    //     executedTime: pr.executedTime,
-    //     restTime: pr.processSize,
-    //     createTime: totalQuantum,
-    //     used: pr.used,
-    //     shouldUse: pr.shouldUse,
-    //   };
-    //   // globalTimer += quantum;
-    //   return ex;
-    // } else {
-
-    // }
   });
-  // console.log("PRCO", process);
   sortByShouldUse(process);
   removeProcess(process);
 }
+function showResult() {
+  console.log("RESULTADO");
+  console.log(finishedProcess);
+}
+/**
+ * > The function creates a process, then executes the process until there is only one process left
+ */
 function main() {
   console.log("PROCESS SCHEDULER");
   createProcess();
-  executeProcess();
-  console.log("FINISHED", finishedProcess);
+  do {
+    executeProcess();
+  } while (process.length > 1);
 }
 
 main();
+
+showResult();
